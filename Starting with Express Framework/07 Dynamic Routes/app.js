8b96a94c5393f5d561c4af0aaf0b3bda648daec5
DIGONTA HALDER <digantahalder14000@gmail.com>
2024-01-06 21:39:10 +0530

    Retrieving data from databases

--- Starting with Express Framework/07 Dynamic Routes/app.js
+++ Starting with Express Framework/07 Dynamic Routes/app.js
@@ -1,22 +1,32 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-const errorsController = require('./controllers/errors.js');
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const db = require("./util/database");
 
-app.set('view engine', 'ejs');
+const errorsController = require("./controllers/errors.js");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.set('views', 'views');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("views", "views");
 
-app.use('/admin', adminRoutes);
+db.execute("SELECT * FROM products")
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorsController.get404);
